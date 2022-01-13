15653638046012661092d8cba8a9cbf8b6c5435f
koko564 <70006867+koko564@users.noreply.github.com>
2022-01-13 22:20:20 +0530

    Delete module4-solution directory

--- module4-solution/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-	var speakWord="GoodBye";
-	var byeSpeaker={};
-	byeSpeaker.speak=function(names){
-		console.log(speakWord+" "+names);
-	}
-	window.byeSpeaker=byeSpeaker;
-})(window);
